0a59c1dbb352bd8afec49baf577924b4c42873fc
EwanBisschoff <ewanbisschoff@gmail.com>
2024-06-28 23:05:38 +0200

    Add game logic: checkWin, switchPlayer, and getCurrentPlayer

--- git/myproject/Code/Tic_Tac_Toe_code/src/TicTacToe.java
+++ git/myproject/Code/Tic_Tac_Toe_code/src/TicTacToe.java
@@ -20,6 +20,30 @@ public class TicTacToe {
             currentPlayer = PLAYER_X;
         }
 
+        public boolean checkWin() {
+            // Check rows and columns
+            for (int i = 0; i < 3; i++) {
+                if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+                        (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+                    return true;
+                }
+            }
+            // Check diagonals
+            if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+                    (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+                return true;
+            }
+            return false;
+        }
+
+        public void switchPlayer() {
+            currentPlayer = (currentPlayer == PLAYER_X) ? PLAYER_O : PLAYER_X;
+        }
+
+        public char getCurrentPlayer() {
+            return currentPlayer;
+        }
+
     }
 
 }
