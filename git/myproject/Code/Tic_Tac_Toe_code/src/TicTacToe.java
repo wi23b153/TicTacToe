b4044603872b7d36ec50fe5fd0664bf49bd4e327
EwanBisschoff <ewanbisschoff@gmail.com>
2024-06-28 22:56:57 +0200

    Add board operations: printBoard, isBoardFull, and makeMove

--- git/myproject/Code/Tic_Tac_Toe_code/src/TicTacToe.java
+++ git/myproject/Code/Tic_Tac_Toe_code/src/TicTacToe.java
@@ -20,6 +20,39 @@ public class TicTacToe {
             currentPlayer = PLAYER_X;
         }
 
-    }
 
+        public void printBoard() {
+            System.out.println("Current Player: " + currentPlayer);
+            System.out.println("▁▁▁▁▁▁");
+            for (int i = 0; i < 3; i++) {
+                System.out.print("|");
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(board[i][j] + "|");
+                }
+                System.out.println();
+            }
+            System.out.println("▔▔▔▔");
+        }
+
+        public boolean isBoardFull() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == EMPTY) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public boolean makeMove(int row, int col) {
+            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != EMPTY) {
+                return false;
+            }
+            board[row][col] = currentPlayer;
+            return true;
+        }
+
+
+    }
 }
